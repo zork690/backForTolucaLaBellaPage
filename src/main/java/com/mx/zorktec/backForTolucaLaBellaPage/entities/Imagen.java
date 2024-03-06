262b0ddce0554c53464e4a18fbfd91392ef3ee65
Cesar Soto <zork_690@hotmail.com>
2024-03-05 18:45:55 -0600

    imagenes_negocios

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/Imagen.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/Imagen.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,40 +18,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-//@Entity
-//@Table(name = "imagenes")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@ToString
-public class Imagen implements Serializable {
+@Entity
+@Table(name = "imagenes_negocios")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class Imagen implements IGenericEntity, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	/*@Id
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
-	@Getter
-	@Setter
 	private int numImagen;
 
-	@Getter
-	@Setter
-	private String imagen;
+	@Column(name="url")
+	private String url;
 
-	@Getter
-	@Setter
-	private String descripcion;
-
-	@Getter
-	@Setter
-	private double precio;
-
-	@Getter
-	@Setter
-	private boolean estatus;
-
-	@Getter
-	@Setter
-	private Timestamp registro; */
+	@ManyToOne
+	@JoinColumn(name="id_negocio", referencedColumnName="id_negocio")
+	private Negocio idNegocio;
 
 }
