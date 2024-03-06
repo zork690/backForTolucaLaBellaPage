b44336edb325480b75047483d976d2498f727270
Cesar Soto <zork_690@hotmail.com>
2024-03-05 18:38:11 -0600

    swagger config

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/SwaggerConfig.java
@@ -0,0 +1,86 @@
+package com.mx.zorktec.backForTolucaLaBellaPage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.google.common.collect.Lists;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer{                                    
+    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.mx.zorktec.backForTolucaLaBellaPage.controllers"))              
+          .paths(PathSelectors.any()).build().apiInfo(apiInfo())
+          .useDefaultResponseMessages(false)
+          .securitySchemes(Lists.newArrayList(apiKey()))
+          .securityContexts(Arrays.asList(securityContext()));                                                 
+    }
+    
+    @Bean
+    public SecurityConfiguration security() {
+    return SecurityConfigurationBuilder.builder().scopeSeparator(",")
+        .additionalQueryStringParams(null)
+        .useBasicAuthenticationWithAccessCodeGrant(false).build();
+    }
+
+
+    private ApiKey apiKey() {
+    return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+    return SecurityContext.builder().securityReferences(defaultAuth())
+        .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope(
+        "global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Arrays.asList(new SecurityReference("apiKey",
+        authorizationScopes));
+    }
+    
+    public ApiInfo apiInfo() {
+        final ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("Toluca la bella Web Site").version("1.0").license("(C) Copyright Test")
+        .description("The API provides a platform to query build test swagger api");
+
+        return builder.build();
+      }
+
+    @Override
+      public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+      }
+}
