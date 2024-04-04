aa411df7e5930a9fc64d22f796263cdce24b5231
Cesar Soto <zork_690@hotmail.com>
2024-04-04 14:07:08 -0600

    creating vo to receive the images

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/ImagenesNegociosVo.java
@@ -0,0 +1,12 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImagenesNegociosVo {
+
+	private String nombre;
+	private String baseContent;
+}
