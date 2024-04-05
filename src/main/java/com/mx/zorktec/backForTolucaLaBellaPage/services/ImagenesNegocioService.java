7fa18c7826c67e3dacd5d206473cd101150b374b
Cesar Soto <zork_690@hotmail.com>
2024-04-05 15:49:53 -0600

    adding negocio as parameter

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/ImagenesNegocioService.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/ImagenesNegocioService.java
@@ -2,10 +2,11 @@ package com.mx.zorktec.backForTolucaLaBellaPage.services;
 
 import java.util.List;
 
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.Negocio;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenesNegociosVo;
 
 public interface ImagenesNegocioService {
 	
-	void processingImagefromNegocio(List<ImagenesNegociosVo> imagenes, String randomId);
+	void processingImagefromNegocio(Negocio negocio, List<ImagenesNegociosVo> imagenes, String randomId);
 
 }
