a5b0ca37bb3a90c48ff7e2ed442d5803d7447818
Cesar Soto <zork_690@hotmail.com>
2024-04-29 16:25:41 -0600

    adding getNegocios method

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/NegocioService.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/NegocioService.java
@@ -1,8 +1,11 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.services;
 
+import java.util.List;
+
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.CredencialesVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.LoginVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.NegocioVo;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.NegociosInfoVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.SettingPassProveedorVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.exceptions.ProveedorException;
 
@@ -11,6 +14,7 @@ public interface NegocioService {
 	public void insertarNegocio(NegocioVo negocio);
 	public void setPassProveedor(CredencialesVo credenciales)throws ProveedorException;
 	public void setPassProveedor(SettingPassProveedorVo credenciales)throws ProveedorException;
+	public List<NegociosInfoVo> getNegocios();
 	//public LoginVo validarProveedor(CredencialesVo credenciales)throws NoSuchFieldException
 	//, IllegalAccessException;
 }
