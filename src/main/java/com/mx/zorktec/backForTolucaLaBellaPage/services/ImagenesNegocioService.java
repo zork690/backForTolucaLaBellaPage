b3ab0f691e40dd2f8a11fa7febb105cd5dbb0cc4
Cesar Soto <zork_690@hotmail.com>
2024-04-29 16:23:48 -0600

    adding getAllImages method

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/ImagenesNegocioService.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/ImagenesNegocioService.java
@@ -2,11 +2,15 @@ package com.mx.zorktec.backForTolucaLaBellaPage.services;
 
 import java.util.List;
 
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.Imagen;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.Negocio;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenesNegociosVo;
 
 public interface ImagenesNegocioService {
 	
-	void processingImagefromNegocio(Negocio negocio, List<ImagenesNegociosVo> imagenes, String randomId);
+	public void processingImagefromNegocio(Negocio negocio
+			, List<ImagenesNegociosVo> imagenes
+			, String randomId);
+	public List<Imagen> getAllImages();
 
 }
