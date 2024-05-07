a91f330be7fefd5d7167f2a6040d3efd1ac6010e
Cesar Soto <zork_690@hotmail.com>
2024-05-06 22:14:40 -0600

    implementing getImagesById

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/ImagenDaoImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/ImagenDaoImpl.java
@@ -1,7 +1,11 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.daos.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.daos.ImagenDao;
@@ -16,6 +20,17 @@ public class ImagenDaoImpl extends GenericDaoImpl<Imagen> implements ImagenDao {
 	protected Class<Imagen> getType() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Optional<List<Imagen>> getImagesById(String id) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(" from Imagen");
+		sb.append(" where id_negocio = ");
+		sb.append(id);
+		String sql = sb.toString();
+		Query<Imagen> query = super.getSession().createQuery(sql, Imagen.class);
+		return Optional.of(query.getResultList());
 	}
 
 }
