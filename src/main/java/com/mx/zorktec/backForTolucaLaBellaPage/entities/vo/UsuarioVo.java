f46b533edb809a13f21c524e330d32ce57baea06
Cesar Soto <zork_690@hotmail.com>
2024-07-24 09:26:23 -0600

    adding new fields

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/UsuarioVo.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/UsuarioVo.java
@@ -1,42 +1,14 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UsuarioVo {
 
-	private Long idUsuario;
-	private String nombreUsuario;
-	private  String contrasena;
-	private Long idPerfil;
-	private String descPerfil;
-	
-	
-	public Long getIdUsuario() {
-		return idUsuario;
-	}
-	public void setIdUsuario(Long idUsuario) {
-		this.idUsuario = idUsuario;
-	}
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-	public Long getIdPerfil() {
-		return idPerfil;
-	}
-	public void setIdPerfil(Long idPerfil) {
-		this.idPerfil = idPerfil;
-	}
-	public String getDescPerfil() {
-		return descPerfil;
-	}
-	public void setDescPerfil(String descPerfil) {
-		this.descPerfil = descPerfil;
-	}
+	private String nombre;
+	private String email;
+	private String telefono;
+	private String password;
 }
