af2532e1cc6801947d0db8eed89bdae4730f7c0c
Cesar Soto <zork_690@hotmail.com>
2024-07-24 09:27:38 -0600

    creating the interface

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/UsuarioDao.java
@@ -0,0 +1,13 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.daos;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.Usuario;
+
+public interface UsuarioDao extends IGenericDao<Usuario>{
+
+	public Optional<List<Usuario>> findAll();
+	public Optional<List<Usuario>> findOnlyValids();
+	public Optional<Usuario> findByEmail(String email);
+}
