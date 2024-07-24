60f78a77a55bf8654896afb53de6d81ca4646fec
Cesar Soto <zork_690@hotmail.com>
2024-07-24 09:22:17 -0600

    adding EnableTransactionManagement annotation

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/BackForTolucaLaBellaPageApplication.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/BackForTolucaLaBellaPageApplication.java
@@ -2,11 +2,13 @@ package com.mx.zorktec.backForTolucaLaBellaPage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 //@EnableEurekaClient
+@EnableTransactionManagement
 public class BackForTolucaLaBellaPageApplication 
 {
     public static void main( String[] args )
