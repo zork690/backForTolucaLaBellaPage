62d0fbdf03947d886cf7572045c058bfaad15cfd
Cesar Soto <zork_690@hotmail.com>
2024-08-07 22:25:33 -0600

    creating vo

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/CredencialesVo.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/CredencialesVo.java
@@ -1,35 +1,14 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
-import lombok.ToString;
-
-@ToString
+@Getter
+@Setter
 public class CredencialesVo {
-
-	@NotBlank(message="Correo requerido")
-	@Email(message = "Correo formato inválido", regexp="^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\\.[a-zA-Z.]{2,5}")
-	private String email;
 	
-	@NotBlank(message="Contraseña requerida")
-	private String pass;
-	
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	
-	
-	
+	private boolean temporary;
+	private String type;
+	private String value;
 	
 }
