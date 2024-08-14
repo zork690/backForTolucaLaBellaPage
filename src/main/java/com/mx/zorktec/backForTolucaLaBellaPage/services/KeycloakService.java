414df6f8747a87fbc426451ab24a8250a2f2e6c5
Cesar Soto <zork_690@hotmail.com>
2024-08-14 01:08:22 -0600

    adding loginUsuario method to be implemented

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/KeycloakService.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/KeycloakService.java
@@ -3,8 +3,11 @@ package com.mx.zorktec.backForTolucaLaBellaPage.services;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.web.client.RestClientException;
 
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.LoginVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.RegistroVo;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ResponseAccessTokenVo;
 
 public interface KeycloakService {
 	public void registrarUsuario(RegistroVo usuario) throws JSONException, RestClientException;
+	public ResponseAccessTokenVo loginUsuario(LoginVo usuario) throws JSONException, RestClientException;
 }
