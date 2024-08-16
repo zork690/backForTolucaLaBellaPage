580c262cd2041e6c025bd119039b0a49ecca4463
Cesar Soto <zork_690@hotmail.com>
2024-08-15 21:20:55 -0600

    implementing Serializable interface

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/UsuarioVo.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/UsuarioVo.java
@@ -1,11 +1,13 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UsuarioVo {
+public class UsuarioVo implements Serializable{
 
 	private String nombre;
 	private String email;
