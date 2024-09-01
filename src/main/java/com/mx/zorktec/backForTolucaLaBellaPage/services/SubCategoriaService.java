6098903d0a28f1912c62a919351d8ea57f81ba31
Cesar Soto <zork_690@hotmail.com>
2024-09-01 15:50:53 -0600

    interface for subcategorias service

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/SubCategoriaService.java
@@ -0,0 +1,12 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.services;
+
+import java.util.List;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.SubCategoriaVo;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.SubCategoria_Vo;
+
+public interface SubCategoriaService {
+
+	public List<SubCategoriaVo> getSubCategorias();
+	public void insertarSubCategoria(SubCategoria_Vo subcategoriaVo);
+}
