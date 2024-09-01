bea413632a3091574124a26badb9c17f312cf4c0
Cesar Soto <zork_690@hotmail.com>
2024-09-01 15:31:56 -0600

    creating dao interface for subcategorias

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/SubCategoriaDao.java
@@ -0,0 +1,7 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.daos;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.SubCategoria;
+
+public interface SubCategoriaDao extends IGenericDao<SubCategoria>{
+
+}
