63afd199fbf83f028d388d2eca971292ca34ac4b
Cesar Soto <zork_690@hotmail.com>
2024-09-01 15:42:43 -0600

    to request for insert or update categorias

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/CategoriaVo.java
@@ -0,0 +1,32 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CategoriaVo implements Serializable{
+	
+	private static final long serialVersionUID = 17546564554L;
+	
+	private int id;
+	
+	@NotBlank(message="Categoría requerida")
+	@Size(max=50, message = "Categoría máximo 50 carácteres")
+	@Pattern(message="Nombre solo letras sin acentos ni ñes", regexp = "^[a-zA-Z ]+$")
+	private String categoria;
+	
+	@NotBlank(message="Imagen requerida")
+	@Pattern(message="Imagen solo letras sin acentos ni ñes", regexp = "^[a-zA-Z ]+$")
+	private String imagen;
+
+	private boolean valid;
+}
