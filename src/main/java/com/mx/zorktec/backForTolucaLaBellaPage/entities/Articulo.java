c7a3e7d77b146621aee65bf485e093d0b0a14979
Cesar Soto <zork_690@hotmail.com>
2024-09-24 22:38:14 -0600

    creating articulo entity

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/Articulo.java
@@ -0,0 +1,39 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "articulos")
+@Getter
+@Setter
+@ToString
+public class Articulo implements IGenericEntity, Serializable{
+	
+	private static final long serialVersionUID = 1560696836789L;
+	
+	@Id
+	@Column(name ="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "descripcion")
+	private String descripcion;
+	
+	@Column(name = "isValid")
+	private boolean valido;
+
+}
