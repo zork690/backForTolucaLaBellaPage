b803bd0258151a2ce57dff5e9b057778f87a5b71
Cesar Soto <zork_690@hotmail.com>
2024-09-24 22:41:54 -0600

    vo for sending images with articulo

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/ImagenArticuloVo.java
@@ -0,0 +1,20 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ImagenArticuloVo implements Serializable{
+
+	private static final long serialVersionUID = 13456890354L;
+	
+	private int id;
+	private String nombre;
+	private String idArticulo;
+	private boolean valid;
+}
