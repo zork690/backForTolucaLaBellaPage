586c451595d00c3908eda6538182b8c1217e7276
Cesar Soto <zork_690@hotmail.com>
2024-09-24 22:39:42 -0600

    vo for validations when articulo is received

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/ArticuloReceivedVo.java
@@ -0,0 +1,34 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ArticuloReceivedVo implements Serializable{
+
+	private static final long serialVersionUID = 1346901L;
+
+	private int id;
+	
+	@NotBlank(message="Nombre de artículo es requerido")
+	@Size(max=128, message = "Nombre artículo máximo 128 carácteres")
+	@Pattern(message="Nombre artículo solo letras o números", regexp = "^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$")
+	private String nombre;
+	
+	@NotBlank(message="Descripción del artículo es requerido")
+	@Size(max=2024, message = "Descripción del artículo máximo 2024 carácteres")
+	@Pattern(message="Descripción del artículo contiene carácteres no permitidos", regexp = "^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ.,\"\\-$()¿?!¡%&\\n\\r“”:; ]+$")
+	private String descripcion;
+	
+	private boolean valid;
+
+}
