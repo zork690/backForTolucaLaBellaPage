ab11e5f6f1cd66f169cae397691a64cbd11b276b
Cesar Soto <zork_690@hotmail.com>
2024-09-26 12:10:12 -0600

    implementing getArticulos, insertarArticulo methods

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/ArticuloServiceImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/ArticuloServiceImpl.java
@@ -8,16 +8,21 @@ import org.springframework.stereotype.Service;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.daos.ArticuloDao;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.Articulo;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.ImagenArticulo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ArticuloReceivedVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ArticuloVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenArticuloVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.services.ArticuloService;
+import com.mx.zorktec.backForTolucaLaBellaPage.services.ImagenesArticuloService;
 
 @Service
 public class ArticuloServiceImpl implements ArticuloService{
 	
 	@Autowired
 	private ArticuloDao articuloDao;
+	
+	@Autowired
+	private ImagenesArticuloService imagenesArticuloService;
 
 	@Override
 	public List<ArticuloVo> getArticulos() {
@@ -25,14 +30,27 @@ public class ArticuloServiceImpl implements ArticuloService{
 
 		List<Articulo> articulos = this.articuloDao.findAll()
 				.orElse(null);
-		//List<Imagen> imagenesList = this.imagenesNegocioService.getAllImages();
+		List<ImagenArticulo> imagenesList = this.imagenesArticuloService.getAllImages();
+		
 		articulos.forEach((articulo)->{
 			ArticuloVo articuloVo = new ArticuloVo();
 			articuloVo.setId(articulo.getId());
 			articuloVo.setNombre(articulo.getNombre());
 			articuloVo.setDescripcion(articulo.getDescripcion());
-			List<ImagenArticuloVo> imagenArticuloVo = new ArrayList<ImagenArticuloVo>();
-			articuloVo.setImagenes(imagenArticuloVo);
+			List<ImagenArticuloVo> listImagenArticuloVo = new ArrayList<ImagenArticuloVo>();
+			
+			imagenesList.forEach((imagen)->{
+				ImagenArticuloVo imagenArticuloVo = new ImagenArticuloVo();
+				if( articulo.getId() == imagen.getArticulo().getId() ) {
+					imagenArticuloVo.setId(imagen.getId());
+					imagenArticuloVo.setIdArticulo(imagen.getArticulo().getId());
+					imagenArticuloVo.setNombre(imagen.getNombre());
+					imagenArticuloVo.setValid(imagen.isValid());
+					listImagenArticuloVo.add(imagenArticuloVo);
+				}
+			});
+
+			articuloVo.setImagenes(listImagenArticuloVo);
 			articuloVo.setValid(articulo.isValido());
 			listArticulosVo.add(articuloVo);
 		});
