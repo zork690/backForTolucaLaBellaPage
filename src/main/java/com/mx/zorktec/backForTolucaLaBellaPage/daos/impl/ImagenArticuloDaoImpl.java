f6949803b2c8b26054ad987840de2ac61f9b9dfd
Cesar Soto <zork_690@hotmail.com>
2024-09-26 11:52:34 -0600

    repository for imagenArticulo entity

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/ImagenArticuloDaoImpl.java
@@ -0,0 +1,17 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.daos.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.daos.ImagenArticuloDao;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.ImagenArticulo;
+
+@Repository
+public class ImagenArticuloDaoImpl extends GenericDaoImpl<ImagenArticulo> implements ImagenArticuloDao {
+
+	@Override
+	protected Class<ImagenArticulo> getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
