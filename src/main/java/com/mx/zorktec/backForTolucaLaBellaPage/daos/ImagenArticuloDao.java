89e075076872152e0963f3f57e346da66392c13e
Cesar Soto <zork_690@hotmail.com>
2024-09-26 11:51:16 -0600

    interface for imagenArticulo entity

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/ImagenArticuloDao.java
@@ -0,0 +1,7 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.daos;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.ImagenArticulo;
+
+public interface ImagenArticuloDao extends IGenericDao<ImagenArticulo>{
+
+}
