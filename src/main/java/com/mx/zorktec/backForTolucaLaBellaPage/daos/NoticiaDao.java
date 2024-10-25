3070b2b3ad3dea2d44d53187215de6c2603efc47
Cesar Soto <zork_690@hotmail.com>
2024-10-24 22:39:33 -0600

    to extend igenericDao Noticia

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/NoticiaDao.java
@@ -0,0 +1,7 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.daos;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.Noticia;
+
+public interface NoticiaDao extends IGenericDao<Noticia> {
+
+}
