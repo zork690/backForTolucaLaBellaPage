71a12d1e470186cd0bc3aecee543185f1ef3f1eb
Cesar Soto <zork_690@hotmail.com>
2024-10-28 15:17:17 -0600

    for generic vo

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/GenericVo.java
@@ -0,0 +1,19 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GenericVo  implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nombre;
+
+}
