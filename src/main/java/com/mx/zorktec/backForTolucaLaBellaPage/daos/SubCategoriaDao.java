c10658f0f39770b2d4e7f419085bee0fb6566625
Cesar Soto <zork_690@hotmail.com>
2024-10-28 19:17:31 -0600

    adding getByCategoriaName method

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/SubCategoriaDao.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/SubCategoriaDao.java
@@ -1,7 +1,10 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.daos;
 
+import java.util.List;
+
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.SubCategoria;
 
 public interface SubCategoriaDao extends IGenericDao<SubCategoria>{
 
+	public List<SubCategoria> getByCategoriaName(String categoria);
 }
