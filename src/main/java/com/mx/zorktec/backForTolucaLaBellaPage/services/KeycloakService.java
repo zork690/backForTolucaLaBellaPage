1894dd7791f25290388bfa8bf93afbf7b95f96a6
Cesar Soto <zork_690@hotmail.com>
2024-12-16 15:48:38 -0600

    feat: adding Exception for registrarUsuario

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/KeycloakService.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/KeycloakService.java
@@ -1,8 +1,5 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.services;
 
-import java.util.List;
-
-import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.web.client.RestClientException;
 
@@ -11,7 +8,7 @@ import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.RegistroVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ResponseAccessTokenVo;
 
 public interface KeycloakService {
-	public void registrarUsuario(RegistroVo usuario) throws JSONException, RestClientException;
+	public void registrarUsuario(RegistroVo usuario) throws JSONException, RestClientException, Exception;
 	public ResponseAccessTokenVo loginUsuario(LoginVo usuario) throws JSONException, RestClientException;
 	public void enviarEmail(String email) throws Exception;
 	public void resetPassword(String email) throws Exception;
