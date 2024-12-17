96d3fee33d90bb36e07d19b74ad7645c6ed4adea
Cesar Soto <zork_690@hotmail.com>
2024-12-16 23:25:52 -0600

    feat: adding insertarCategoria method

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/controllers/PanelSociosController.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/controllers/PanelSociosController.java
@@ -22,10 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.SimpleResponse;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ArticuloReceivedVo;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.CategoriaVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenesArticuloVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ListUpdateArticuloImagesVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.NoticiaVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.services.ArticuloService;
+import com.mx.zorktec.backForTolucaLaBellaPage.services.CategoriaService;
 import com.mx.zorktec.backForTolucaLaBellaPage.services.ImagenesArticuloService;
 import com.mx.zorktec.backForTolucaLaBellaPage.services.NoticiaService;
 
@@ -44,6 +46,9 @@ public class PanelSociosController {
 	
 	@Autowired
 	private NoticiaService noticiaService;
+	
+	@Autowired
+	private CategoriaService categoriaService;
 
 	@PostMapping("/articulos/insertar")
 	public ResponseEntity<SimpleResponse> insertarArticulo(@Validated @RequestBody ArticuloReceivedVo articulo){
@@ -131,6 +136,29 @@ public class PanelSociosController {
 			return new ResponseEntity<>(srResult,HttpStatus.BAD_REQUEST);
 		}catch (DataAccessException e) {
 			LOG.error("Ocurrio un error al guardar la noticia: {}", e.getLocalizedMessage());
+			srResult.setError("Existe un problema accesando a la base.");
+			return new ResponseEntity<>(srResult,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		srResult.setMessage("OK");
+		return new ResponseEntity<>(srResult, HttpStatus.OK);
+	}
+	
+	
+	@PostMapping("/categorias/crearCategoria")
+	public ResponseEntity<SimpleResponse> insertarCategoria(@Validated @RequestBody CategoriaVo categoria){
+		SimpleResponse srResult = new SimpleResponse();
+
+		try {
+			LOG.info("Categoria enviada: {}", categoria);
+			this.categoriaService.insertarCategoria(categoria);
+			srResult.setResult("Categoría insertada o actualizada correctamente");
+		} 
+		catch(org.springframework.dao.DataIntegrityViolationException cExc) {
+			LOG.info("Violación de reglas al insertar o actualizar categoría: {}" ,cExc.getLocalizedMessage());
+			srResult.setError("Violación de reglas al insertar o actualizar categoría.");
+			return new ResponseEntity<>(srResult,HttpStatus.BAD_REQUEST);
+		}catch (DataAccessException e) {
+			LOG.error("Ocurrio un error al guardar la categoría: {}", e.getLocalizedMessage());
 			srResult.setError("Existe un problema accesando a la base.");
 			return new ResponseEntity<>(srResult,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
