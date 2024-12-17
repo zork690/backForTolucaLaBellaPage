eba467c995cc15eabfb2bb0e3ef875f2889084fc
Cesar Soto <zork_690@hotmail.com>
2024-12-16 22:47:49 -0600

    feat: adding private methods to add role to user when is registered

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/KeycloakServiceImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/KeycloakServiceImpl.java
@@ -13,6 +13,7 @@ import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpEntity;
@@ -44,10 +45,10 @@ public class KeycloakServiceImpl implements KeycloakService {
 
 	@Value("${keycloak.credentials.secret}")
 	private String clientSecret;
-	
+
 	@Value("${zorktech.keycloak.username}")
 	private String userName;
-	
+
 	@Value("${zorktech.keycloak.password}")
 	private String password;
 
@@ -56,24 +57,33 @@ public class KeycloakServiceImpl implements KeycloakService {
 
 	@Value("${zorktech.keycloak.url.token}")
 	private String tokenUrl;
-	
+
 	@Value("${zorktech.keycloak.url.register}")
 	private String urlRegister;
-	
+
 	/*@Value("${zorktech.keycloak.url.send.mail}")
 	private String urlSendMail;*/
-	
+
 	@Value("${zorktech.keycloak.url.execute.actions.mail}")
 	private String urlSendMail;
-	
+
 	@Value("${keycloak.realm}")
 	private String realm;
 
+	@Value("${zorktech.keycloak.url.get.resource.id}")
+	private String gettingClientIdUrl;
+
+	@Value("${zorktech.keycloak.url.get.roles.info}")
+	private String gettingRolesInfoUrl;
+	
+	@Value("${zorktech.keycloak.url.set.role.user}")
+	private String settingRoleUrl;
+
 	@Autowired
-    Keycloak keycloak;
+	Keycloak keycloak;
 
 	@Override
-	public void registrarUsuario(RegistroVo usuario) throws JSONException, RestClientException {
+	public void registrarUsuario(RegistroVo usuario) throws JSONException, RestClientException, Exception {
 		UsuarioKeycloakVo usuarioKeycloak = new UsuarioKeycloakVo();
 		usuarioKeycloak.setUsername(usuario.getUsuario());
 		usuarioKeycloak.setFirstName(usuario.getNombre());
@@ -88,10 +98,13 @@ public class KeycloakServiceImpl implements KeycloakService {
 		List<CredencialesVo> credencialesList = new ArrayList<CredencialesVo>();
 		credencialesList.add(credenciales);
 		usuarioKeycloak.setCredentials(credencialesList);
-		
+
 		this.sendRegistrationRequest(this.generateAccessToken(), usuarioKeycloak);
+		String id = this.getClientIdRequest();
+		JSONObject roleInfo = this.gettingRolesInfo(id);
+		this.sendSettingRoleRequest(usuarioKeycloak, roleInfo);
 	}
-	
+
 	@Override
 	public ResponseAccessTokenVo loginUsuario(LoginVo usuario) throws JSONException, RestClientException {
 		LOG.info("Haciendo login ...");
@@ -111,11 +124,11 @@ public class KeycloakServiceImpl implements KeycloakService {
 				);
 		responseAcessTokenVo.setSessionState(responseJsonObject.getString("session_state"));
 		responseAcessTokenVo.setScope(responseJsonObject.getString("scope"));
-		
+
 		return responseAcessTokenVo;
-		
+
 	}
-	
+
 	@Override
 	public void enviarEmail(String email) throws Exception {
 		List<UserRepresentation> userList = this.findUserByEmail(email);
@@ -126,7 +139,7 @@ public class KeycloakServiceImpl implements KeycloakService {
 		this.sendConfirmationEmailRequest(this.generateAccessToken(), userRepresentation.getId()
 				, new String[] {"VERIFY_EMAIL"});
 	}
-	
+
 	@Override
 	public void resetPassword(String email) throws Exception {
 		List<UserRepresentation> userList = this.findUserByEmail(email);
@@ -137,34 +150,34 @@ public class KeycloakServiceImpl implements KeycloakService {
 		this.sendConfirmationEmailRequest(this.generateAccessToken(), userRepresentation.getId()
 				, new String[] {"UPDATE_PASSWORD"});
 	}
-	
+
 	private List<UserRepresentation> findUserByEmail(String email){
 		boolean exact = true;
 		List<UserRepresentation> user = this.keycloak.realm(this.realm)
 				.users()
 				.searchByEmail(email, exact);
-		
+
 		LOG.info("User with email {}", user.stream()
 				.map(u->u.getEmail())
 				.collect(Collectors.toList())
 				);
-		
+
 		LOG.info("ID: {}", user.stream()
 				.map(u->
-					u.getId()
-					)
+				u.getId()
+						)
 				.collect(Collectors.toList())
 				);
 		return user;
 	}
-	
+
 	private void sendRegistrationRequest(String token, UsuarioKeycloakVo usuario) throws RestClientException {
 		RestTemplate restTemplateRegister = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.set(HttpHeaders.ACCEPT, "application/json");
 		headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
 		headers.set(HttpHeaders.AUTHORIZATION, "Bearer "+token);
-		
+
 		Gson gson = new Gson();
 		String gsonString = gson.toJson(usuario);
 		HttpEntity<String> entity = new HttpEntity<String>(gsonString.toString(),headers);
@@ -172,36 +185,36 @@ public class KeycloakServiceImpl implements KeycloakService {
 				, HttpMethod.POST
 				, entity
 				, String.class).getBody();
-		
+
 		LOG.info("Respuesta del registro: {}", response);
 	}
-	
+
 	private void sendConfirmationEmailRequest(String token, String idUser
 			,String[] bodyArray) throws RestClientException {
 		Map<String, String> urlParamsMap = new HashMap<String, String>();
 		urlParamsMap.put("id", idUser);
-		
+
 		UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromUriString(this.urlSendMail);	
 		String url  = urlBuilder.buildAndExpand(urlParamsMap).toUriString();
-		
+
 		LOG.info("URL to send email: {}", url);
-		
+
 		RestTemplate restTemplateRegister = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.set(HttpHeaders.ACCEPT, "application/json");
 		headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
 		headers.set(HttpHeaders.AUTHORIZATION, "Bearer "+token);		
-		
+
 		LOG.info("body Array: {}", Arrays.toString(bodyArray));
-		
+
 		HttpEntity<String[]> entity = new HttpEntity<String[]>(bodyArray, headers);
 		restTemplateRegister.exchange(url
 				, HttpMethod.PUT
 				, entity
 				, String[].class).getBody();
 	}
-	
-	
+
+
 	private String generateAccessToken() throws JSONException, RestClientException {
 		String token = null;
 		RestTemplate restTemplateToken = new RestTemplate();
@@ -227,8 +240,8 @@ public class KeycloakServiceImpl implements KeycloakService {
 		LOG.info("Keycloak token generado: {}", token);
 		return token;
 	}
-	
-	
+
+
 	private JSONObject generateAccessToken(LoginVo usuario) throws JSONException, RestClientException {
 		RestTemplate restTemplateToken = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
@@ -250,5 +263,107 @@ public class KeycloakServiceImpl implements KeycloakService {
 
 		jsonObject = new JSONObject(jsonResponse);
 		return jsonObject;
+	}
+
+	private void sendSettingRoleRequest(UsuarioKeycloakVo usuarioKeycloak, JSONObject roleInfo) throws Exception {
+		LOG.info("sending request to set user role...");
+		
+		List<UserRepresentation> userList = this.findUserByEmail(usuarioKeycloak.getEmail());
+		if(userList.isEmpty()) {
+			throw new Exception("Email no ha sido encontrado");
+		}
+		UserRepresentation userRepresentation = userList.stream().findFirst().orElse(null);
+		
+		RestTemplate restTemplateRegister = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.ACCEPT, "application/json");
+		headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+		headers.set(HttpHeaders.AUTHORIZATION, "Bearer "+this.generateAccessToken());
+		
+		
+
+		Map<String, String> urlParamsMap = new HashMap<String, String>();
+		urlParamsMap.put("userId", userRepresentation.getId());
+		urlParamsMap.put("clientUid", roleInfo.getString("containerId"));
+
+		UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromUriString(this.settingRoleUrl);	
+		String url  = urlBuilder.buildAndExpand(urlParamsMap).toUriString();
+		
+		
+		JSONArray dataList = new JSONArray();
+		dataList.put(roleInfo);
+		LOG.info("Data to role: {}", dataList);
+		
+		HttpEntity<String> entity = new HttpEntity<String>(dataList.toString()
+				,headers);
+		String response = restTemplateRegister.exchange(url
+				, HttpMethod.POST
+				, entity
+				, String.class).getBody();
+
+		LOG.info("RESPONSE: {}", response);
+		
+	}
+
+	private String getClientIdRequest() throws JSONException {
+		LOG.info("getting client id ...");
+		RestTemplate restTemplateRegister = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.ACCEPT, "application/json");
+		headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+		headers.set(HttpHeaders.AUTHORIZATION, "Bearer "+this.generateAccessToken());
+
+		Map<String, String> urlParamsMap = new HashMap<String, String>();
+		urlParamsMap.put("sourceId", this.clientId);
+
+		UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromUriString(this.gettingClientIdUrl);	
+		String url  = urlBuilder.buildAndExpand(urlParamsMap).toUriString();
+
+		HttpEntity<String> entity = new HttpEntity<String>("",headers);
+		String response = restTemplateRegister.exchange(url
+				, HttpMethod.GET
+				, entity
+				, String.class).getBody();
+
+		LOG.info("RESPONSE: {}", response);
+		JSONArray jsonArray = new JSONArray(response);
+		JSONObject jsonObject = jsonArray.getJSONObject(0);
+		String id = jsonObject.getString("id");		
+		LOG.info("id client: {}", id);
+		return id;
+	}
+
+	private JSONObject gettingRolesInfo(String sourceId) throws JSONException {
+		String roleToSearch = "user";
+		JSONObject roleInfo = null;
+		LOG.info("getting roles info ...");
+		RestTemplate restTemplateRegister = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.ACCEPT, "application/json");
+		headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+		headers.set(HttpHeaders.AUTHORIZATION, "Bearer "+this.generateAccessToken());
+
+		Map<String, String> urlParamsMap = new HashMap<String, String>();
+		urlParamsMap.put("clientId", sourceId);
+
+		UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromUriString(this.gettingRolesInfoUrl);	
+		String url  = urlBuilder.buildAndExpand(urlParamsMap).toUriString();
+
+		HttpEntity<String> entity = new HttpEntity<String>("",headers);
+		String response = restTemplateRegister.exchange(url
+				, HttpMethod.GET
+				, entity
+				, String.class).getBody();
+
+		LOG.info("RESPONSE: {}", response);
+		JSONArray jsonArray = new JSONArray(response);
+		for(int i=0; i<jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+			if(jsonObject.getString("name").equals(roleToSearch)) {
+				LOG.info("ROLE INFO: {}", jsonObject);
+				roleInfo = jsonObject;
+			}
+		}
+		return roleInfo;
 	}
 }
