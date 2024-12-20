ff527e505707e115d8c93c9b16c9039f244af9d3
Cesar Soto <zork_690@hotmail.com>
2024-12-20 15:23:18 -0600

    feat: implementing getInfoFromToken

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/utilities/Utilities.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/utilities/Utilities.java
@@ -2,11 +2,18 @@ package com.mx.zorktec.backForTolucaLaBellaPage.utilities;
 
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.time.Year;
-import java.util.Calendar;
-import java.util.Date;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.TokenPayloadVo;
 
 public class Utilities {
+	
+	private static final Logger LOG = LogManager.getLogger(Utilities.class);
 
 	public static final String limpiaCadena(String texto) {
 
@@ -26,6 +33,21 @@ public class Utilities {
 	public static final String generateTimeStampString() {
 		Timestamp ts = Timestamp.from(Instant.now());
 		return String.valueOf(ts.getTime());
+	}
+	
+	public static final void getInfoFromToken(String token) {
+		String t = token.replace("Bearer", "");
+		LOG.info("Token: {}", t);
+		String[] chunks = t.split("\\.");
+		Base64 decoder = new Base64(true);
+		String header = new String(decoder.decode(chunks[0]));
+		String payload = new String(decoder.decode(chunks[1]));
+		LOG.info("PAYLOAD: {}", payload);
+		JsonObject o = new Gson().fromJson(payload, JsonObject.class);
+		TokenPayloadVo tokenPayload = new TokenPayloadVo();
+		tokenPayload.setEmail(o.get("email").toString());
+		LOG.info("Email from payload: {}", tokenPayload.getEmail());
+		
 	}
 
 }
