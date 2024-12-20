9045863a4ad046634fb9fdc376706efe86460d6c
Cesar Soto <zork_690@hotmail.com>
2024-12-20 13:16:13 -0600

    feat: deleting crear and unused imports

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/controllers/SubcategoriaRestController.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/controllers/SubcategoriaRestController.java
@@ -7,24 +7,19 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.SimpleResponse;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.SubCategoriaVo;
-import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.SubCategoria_Vo;
 import com.mx.zorktec.backForTolucaLaBellaPage.services.SubCategoriaService;
 
 @RestController
@@ -64,28 +59,6 @@ public class SubcategoriaRestController {
 			response.setError(error.getMessage());
 			return new ResponseEntity<SimpleResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-	}
-	
-	@PostMapping("/crear")
-	public ResponseEntity<SimpleResponse> insertarSubCategoria(@Validated @RequestBody SubCategoria_Vo subcategoria){
-		SimpleResponse srResult = new SimpleResponse();
-
-		try {
-			LOG.info("SubCategoria enviada: {}", subcategoria);
-			this.subcategoriaService.insertarSubCategoria(subcategoria);
-			srResult.setResult("Subcategoría insertada o actualizada correctamente");
-		} 
-		catch(org.springframework.dao.DataIntegrityViolationException cExc) {
-			LOG.info("Violación de reglas al insertar o actualizar subcategoría: {} " ,cExc.getLocalizedMessage());
-			srResult.setError("Violación de reglas al insertar o actualizar subcategoría.");
-			return new ResponseEntity<>(srResult,HttpStatus.BAD_REQUEST);
-		}catch (DataAccessException e) {
-			LOG.error("Ocurrio un error al guardar la categoría: {}", e.getLocalizedMessage());
-			srResult.setError("Existe un problema accesando a la base.");
-			return new ResponseEntity<>(srResult,HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		srResult.setMessage("OK");
-		return new ResponseEntity<>(srResult, HttpStatus.OK);
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
