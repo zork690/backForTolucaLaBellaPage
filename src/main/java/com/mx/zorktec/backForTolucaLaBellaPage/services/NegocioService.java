8586f65684122639f93a16f5dbdf648655f98b96
Cesar Soto <zork_690@hotmail.com>
2024-12-24 21:59:32 -0600

    feat: adding getNegociosbyUser

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/NegocioService.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/NegocioService.java
@@ -3,11 +3,10 @@ package com.mx.zorktec.backForTolucaLaBellaPage.services;
 import java.util.List;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.CredencialesVo;
-import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenNegocioVo;
-import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.LoginVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.NegocioVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.NegociosInfoVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.SettingPassProveedorVo;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.TokenPayloadVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.UpdateNegocioVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.exceptions.ProveedorException;
 
@@ -20,7 +19,6 @@ public interface NegocioService {
 	public List<NegociosInfoVo> getNegociosTodos();
 	public NegociosInfoVo getNegocioById(String id) throws Exception;
 	public void actualizarNegocio(UpdateNegocioVo negocio) throws NullPointerException;
-	//public LoginVo validarProveedor(CredencialesVo credenciales)throws NoSuchFieldException
-	//, IllegalAccessException;
 	public List<NegociosInfoVo> getNegociosBySubCategoria(String subcategoria);
+	public List<NegociosInfoVo> getNegociosbyUser(TokenPayloadVo tokenInfo);
 }
