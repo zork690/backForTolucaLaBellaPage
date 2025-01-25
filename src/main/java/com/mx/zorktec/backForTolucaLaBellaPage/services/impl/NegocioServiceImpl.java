254414aa5a9309be02f8263cd7e4036ac6dbae7b
Cesar Soto <zork_690@hotmail.com>
2025-01-24 20:35:29 -0600

    feat: changing actualizarNegocio deleting negocio and ubicacion daos and adding getting user from tokenInfo

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/NegocioServiceImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/NegocioServiceImpl.java
@@ -36,51 +36,51 @@ import com.mx.zorktec.backForTolucaLaBellaPage.utilities.Utilities;
 
 @Service
 public class NegocioServiceImpl implements NegocioService{
-	
+
 	private static final Logger LOG = LogManager.getLogger(NegocioServiceImpl.class);
 
 	@Autowired
 	private NegocioDao negocioDao;
-	
+
 	@Autowired
 	private UbicacionesDao ubicacionesDao;
-	
+
 	@Autowired
 	private ImagenesNegocioService imagenesNegocioService;
-	
+
 	@Autowired
 	private ImagenDao imagenesDao;
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	//@Autowired
 	//private PermisosPerfilesDAO permisosDao;
-	
+
 	//@Autowired
 	//private EnviaEmailService enviaEmailService;
-	
+
 	@Value("${secure.secretKey}")
 	private String secretKey;
-	
+
 	@Value("${secure.setJTI}")
 	private String setJTI;
-	
+
 	@Value("${secure.timeTokenMs}")
 	private int timeTokenMs;
-	
+
 	@Override
 	@Transactional
 	public void insertarNegocio(NegocioVo negocio) {
 		String randomId = Utilities.generateIdForClient();
-		
+
 		this.usuarioService.insertarUsuario(this.usuarioService.generateUsuarioVo(negocio));
 		Usuario usuario = this.usuarioService.getByEmail(negocio.getCorreo());
-		
+
 		if(usuario == null) {
 			throw new NullPointerException("Usuario no encontrado por el email: "+negocio.getCorreo());
 		}
-		
+
 		LOG.info("RANDOM STRING: "+randomId);
 		Negocio p = new Negocio();
 		p.setIdNegocio(randomId);
@@ -90,48 +90,43 @@ public class NegocioServiceImpl implements NegocioService{
 		s.setId( Integer.valueOf( negocio.getIdSubcategoria() ) );
 		p.setSubCategoria( s );
 		p.setDescripcion(negocio.getDescripcionComercial());
-		
+
 		Ubicacion u = this.ubicacionesDao
 				.findById(Ubicacion.class, Integer.parseInt(negocio.getIdUbicacion())).orElse(null);
 		p.setIdUbicacion(u);
-		
+
 		p.setNombreEmpresa(negocio.getNombreEmpresa());
 		p.setNumeroExterior(negocio.getNumeroExterior());
 		p.setTelefono(negocio.getTelefono());
-		
+
 		this.negocioDao.saveOrUpdate(p);
 		this.imagenesNegocioService.processingImagefromNegocio(p, negocio.getImagenes(), randomId);
 		//this.enviaEmailService.enviarEmail(proveedor.getCorreo());
 	}
-	
-	@Override //TODO REFACTORIZAR PARA VER SI PODEMOS QUITAR EL FIND BY ID DE NEGOCIODAO Y UBICACIONESDAO
-	public void actualizarNegocio(UpdateNegocioVo negocio) throws NullPointerException {
-		Optional<Negocio> p = this.negocioDao.findById(Negocio.class, negocio.getId());
-		
-		if(p == null) {
-			throw new NullPointerException("negocio no encontrado");
-		}
-			
-		
-		if(p.isPresent()) {
-			Negocio n = p.get();
+
+	@Override
+	public void actualizarNegocio(UpdateNegocioVo negocio, TokenPayloadVo tokenInfo) throws NullPointerException {
+		Usuario usuario = this.usuarioService.getByEmail(tokenInfo.getEmail());
+		LOG.info("USUARIO ID: {}", usuario.getIdUsuario());
+		Negocio n = new Negocio();
+		SubCategoria s = new SubCategoria();
+		s.setId(Integer.valueOf(negocio.getIdSubcategoria()));
+		Ubicacion ubicacion = new Ubicacion();
+		ubicacion.setId(Integer.valueOf(negocio.getIdUbicacion()));
+		n.setIdNegocio(negocio.getId());
+		n.setIdUsuario(usuario);
 		n.setCalle(negocio.getCalle());
+		n.setSubCategoria(s);
 		n.setDescripcion(negocio.getDescripcionComercial());
-		
-		Ubicacion u = this.ubicacionesDao
-				.findById(Ubicacion.class, Integer.parseInt(negocio.getIdUbicacion())).orElse(null);
-		n.setIdUbicacion(u);
-		
+		n.setIdUbicacion(ubicacion);
 		n.setNombreEmpresa(negocio.getNombreEmpresa());
 		n.setNumeroExterior(negocio.getNumeroExterior());
 		n.setTelefono(negocio.getTelefono());
-		n.setValido(negocio.isValido());
-		
-		this.negocioDao.saveOrUpdate(n);
-		}
-		
+
+		this.negocioDao.saveOrUpdate(n);	
+
 	}
-	
+
 	@Override
 	public void insertarNegocioUserLoggued(UpdateNegocioVo negocio, TokenPayloadVo tokenInfo) {
 		Usuario usuario = this.usuarioService.getByEmail(tokenInfo.getEmail());
@@ -151,22 +146,22 @@ public class NegocioServiceImpl implements NegocioService{
 		bussiness.setNombreEmpresa(negocio.getNombreEmpresa());
 		bussiness.setNumeroExterior(negocio.getNumeroExterior());
 		bussiness.setTelefono(negocio.getTelefono());
-		
+
 		this.negocioDao.saveOrUpdate(bussiness);
 	}
-	
+
 	@Override
 	public List<NegociosInfoVo> getNegocios() {
 		List<NegociosInfoVo> listNegociosVo = new ArrayList<NegociosInfoVo>();
-		
+
 		List<Negocio> negocios = this.negocioDao.findOnlyValids()
 				.orElse(null);
 		List<Imagen> imagenesList = this.imagenesNegocioService.getOnlyValidImages();
-		
+
 		negocios.forEach((negocio)->{
 			NegociosInfoVo negociosVo = new NegociosInfoVo();
 			List<ImagenNegocioVo> listImagenNegocio = new ArrayList<ImagenNegocioVo>();
-			
+
 			negociosVo.setId(negocio.getId());
 			negociosVo.setIdNegocio(negocio.getIdNegocio());
 			negociosVo.setNombre(negocio.getIdUsuario().getNombre());
@@ -175,13 +170,13 @@ public class NegocioServiceImpl implements NegocioService{
 			negociosVo.setUbicacion(negocio.getIdUbicacion());
 			negociosVo.setDescripcion(negocio.getDescripcion());
 			negociosVo.setCalle(negocio.getCalle());
-			
+
 			this.setSubcategoriaNegocio(negocio, negociosVo);
-			
+
 			negociosVo.setNombrEmpresa(negocio.getNombreEmpresa());
 			negociosVo.setNumeroExterior(negocio.getNumeroExterior());
 			negociosVo.setValid(negocio.isValido());
-			
+
 			imagenesList.forEach((imagen)->{
 				ImagenNegocioVo imagenNegocioVo = new ImagenNegocioVo();
 				if(negocio.getIdNegocio().equals(imagen.getIdNegocio().getIdNegocio())) {
@@ -192,14 +187,14 @@ public class NegocioServiceImpl implements NegocioService{
 					listImagenNegocio.add(imagenNegocioVo);
 				}
 			});
-			
+
 			negociosVo.setImagenes(listImagenNegocio);
-			
+
 			listNegociosVo.add(negociosVo);
 		});
 		return listNegociosVo;
 	}
-	
+
 	@Override
 	public List<NegociosInfoVo> getNegociosTodos() {
 		List<NegociosInfoVo> listNegociosVo = new ArrayList<NegociosInfoVo>();
@@ -220,9 +215,9 @@ public class NegocioServiceImpl implements NegocioService{
 			negociosVo.setUbicacion(negocio.getIdUbicacion());
 			negociosVo.setDescripcion(negocio.getDescripcion());
 			negociosVo.setCalle(negocio.getCalle());
-			
+
 			this.setSubcategoriaNegocio(negocio, negociosVo);
-			
+
 			negociosVo.setNombrEmpresa(negocio.getNombreEmpresa());
 			negociosVo.setNumeroExterior(negocio.getNumeroExterior());
 			negociosVo.setValid(negocio.isValido());
@@ -244,7 +239,7 @@ public class NegocioServiceImpl implements NegocioService{
 		});
 		return listNegociosVo;
 	}
-	
+
 	@Override
 	public NegociosInfoVo getNegocioById(String id) throws Exception {
 		Optional<Negocio> negocio = this.negocioDao.findById(Negocio.class, id);
@@ -268,9 +263,9 @@ public class NegocioServiceImpl implements NegocioService{
 				negocioVo.setUbicacion(negocio.get().getIdUbicacion());
 				negocioVo.setDescripcion(negocio.get().getDescripcion());
 				negocioVo.setCalle(negocio.get().getCalle());
-				
+
 				this.setSubcategoriaNegocio(negocio.get(), negocioVo);
-				
+
 				negocioVo.setNombrEmpresa(negocio.get().getNombreEmpresa());
 				negocioVo.setNumeroExterior(negocio.get().getNumeroExterior());
 				negocioVo.setImagenes(listImagenNegocio);
@@ -283,22 +278,22 @@ public class NegocioServiceImpl implements NegocioService{
 		}else {
 			throw new Exception("Null exception");
 		}
-		
+
 	}
-	
+
 	@Override
 	public List<NegociosInfoVo> getNegociosBySubCategoria(String subcategoria) {
 		List<Negocio> negocios = this.negocioDao.getBySubcategoria(subcategoria);
 		List<NegociosInfoVo> negociosVo = new ArrayList<NegociosInfoVo>();
 		List<Imagen> imagenesList = this.imagenesNegocioService.getOnlyValidImages();
-		
+
 		negocios.forEach((negocio)->{
 			NegociosInfoVo iNegocioVo = new NegociosInfoVo();
 			List<ImagenNegocioVo> listImagenNegocio = new ArrayList<ImagenNegocioVo>();
 			iNegocioVo.setNombrEmpresa(negocio.getNombreEmpresa());
 			iNegocioVo.setValid(negocio.isValido());
 			iNegocioVo.setIdNegocio(negocio.getIdNegocio());
-			
+
 			imagenesList.forEach((imagen)->{
 				ImagenNegocioVo imagenNegocioVo = new ImagenNegocioVo();
 				if(negocio.getIdNegocio().equals(imagen.getIdNegocio().getIdNegocio())) {
@@ -309,47 +304,47 @@ public class NegocioServiceImpl implements NegocioService{
 					listImagenNegocio.add(imagenNegocioVo);
 				}
 			});
-			
+
 			iNegocioVo.setImagenes(listImagenNegocio);
 			negociosVo.add(iNegocioVo);
-			
+
 		});
-		
+
 		return negociosVo;
 	}
 
 	@Override
 	public void setPassProveedor(CredencialesVo credenciales) throws ProveedorException {
-		
+
 		//BUSCAR EMAIL EN LA BASE Y REVISAR QUE YA ESTE VERIFICADO
 		/*Proveedor p = this.proveedorDao.findByEmail(credenciales);
-		
+
 		//SI PROVEEDOR ES NULO O NO ESTA VERIFICADO LANZAR ProveedorException("Proveedor no encontrado o no verificado")
 		if(p == null) {
 			throw new ProveedorException("Proveedor no encontrado o no verificado");
 		}else {
-			
+
 			if(!p.isValidated()) {
 				throw new ProveedorException("Proveedor no encontrado o no verificado");
 			}
-			
+
 			LOG.info("Agregando pass a proveedor: "+p.getCorreo());
 			//this.proveedorDao.saveOrUpdate(p);
 		}*/
-		
-		
+
+
 	}
-	
+
 	@Override
 	public List<NegociosInfoVo> getNegociosbyUser(TokenPayloadVo tokenInfo) {
 		List<Negocio> negocios = this.negocioDao.getByUser(tokenInfo.getEmail());
 		List<NegociosInfoVo> negociosVo = new ArrayList<NegociosInfoVo>();
 		List<Imagen> imagenesList = this.imagenesNegocioService.getAllImages();
-		
+
 		negocios.forEach((negocio)->{
 			NegociosInfoVo iNegocioVo = new NegociosInfoVo();
 			List<ImagenNegocioVo> listImagenNegocio = new ArrayList<ImagenNegocioVo>();
-			
+
 			iNegocioVo.setId(negocio.getId());
 			iNegocioVo.setIdNegocio(negocio.getIdNegocio());
 			iNegocioVo.setNombre(negocio.getIdUsuario().getNombre());
@@ -358,11 +353,11 @@ public class NegocioServiceImpl implements NegocioService{
 			iNegocioVo.setUbicacion(negocio.getIdUbicacion());
 			iNegocioVo.setDescripcion(negocio.getDescripcion());
 			iNegocioVo.setCalle(negocio.getCalle());
-			
+
 			this.setSubcategoriaNegocio(negocio, iNegocioVo);
 			iNegocioVo.setNombrEmpresa(negocio.getNombreEmpresa());
 			iNegocioVo.setNumeroExterior(negocio.getNumeroExterior());
-			
+
 			iNegocioVo.setValid(negocio.isValido());			
 
 			imagenesList.forEach((imagen)->{
@@ -375,19 +370,19 @@ public class NegocioServiceImpl implements NegocioService{
 					listImagenNegocio.add(imagenNegocioVo);
 				}
 			});
-			
+
 			iNegocioVo.setImagenes(listImagenNegocio);
 			negociosVo.add(iNegocioVo);
 		});
-		
+
 		return negociosVo;
 	}
-	
+
 
 	@Override
 	public void setPassProveedor(SettingPassProveedorVo credenciales) throws ProveedorException {
 		LOG.info("Setting Pass a Proveedor: ");
-		
+
 		//crear el método en los daos que busca al usuario por codigo
 	}
 
@@ -397,21 +392,21 @@ public class NegocioServiceImpl implements NegocioService{
 		Usuario usuarioValidar = proveedorDao.validarProveedor(credenciales);
 		UsuarioVo usuarioVo = new UsuarioVo();
 		LoginVo loginVo = new LoginVo();
-		
+
 		if (usuarioValidar != null) {
 			String s = new String(usuarioValidar.getPass());
 			byte[] decoded = Base64.getDecoder().decode(s.trim());
 			String contrasenaBD = new String(decoded);
 			String contraEnviada = credenciales.getPass().trim();
-			
+
 			LOG.info(contraEnviada);
-			
+
 			if (!contrasenaBD.equals(contraEnviada)) {
 				usuarioVo.setIdUsuario(usuarioValidar.getId());
 				usuarioVo.setIdPerfil(usuarioValidar.getIdPerfil().getId());
 				usuarioVo.setNombreUsuario(usuarioValidar.getNombre());
 				usuarioVo.setContrasena(s.trim());
-				
+
 				PermisosVo permisosVo = new PermisosVo();
 				List<PermisosPerfil> permisos = permisosDao
 						.obtienePermisosByPerfil(usuarioValidar.getIdPerfil().getId());
@@ -421,13 +416,13 @@ public class NegocioServiceImpl implements NegocioService{
 								, permisosPerfiles.getBandera());
 					}
 				}
-				
+
 				loginVo.setEstatus(true);
 				loginVo.setUsuario(usuarioVo);
 				loginVo.setPermisos(permisosVo);
 				loginVo.setMsgDesripcion("Inicio de sesión correcto");
 				loginVo.setToken(this.setJWTToken(usuarioVo.getNombreUsuario()));
-				
+
 			}else {
 				loginVo.setEstatus(false);
 				loginVo.setUsuario(null);
@@ -440,17 +435,17 @@ public class NegocioServiceImpl implements NegocioService{
 			loginVo.setPermisos(null);
 			loginVo.setMsgDesripcion("El usuario no existe");
 		}
-		
+
 		return loginVo;
 	}*/
-	
+
 	/*private String setJWTToken(String username) {
-		
+
 		LOG.info("GENERATING JWT TOKEN...");
-		
+
 		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
 				.commaSeparatedStringToAuthorityList("ROLE_USER");
-		
+
 		@SuppressWarnings("deprecation")
 		String token = Jwts
 				.builder()
@@ -468,18 +463,18 @@ public class NegocioServiceImpl implements NegocioService{
 		return "Bearer " + token;
 		return "";
 	}*/
-	
+
 	private void setSubcategoriaNegocio(Negocio negocio, NegociosInfoVo negociosVo) {
 		CategoriaSubCategoriaVo cSVo = new CategoriaSubCategoriaVo();
 		cSVo.setId(negocio.getSubCategoria().getCategoria().getId());
 		cSVo.setNombre(negocio.getSubCategoria().getCategoria().getCategoria());
-		
+
 		SubCategoriaVo sVo = new SubCategoriaVo();
 		sVo.setId(negocio.getSubCategoria().getId());
 		sVo.setCategoria(cSVo);
 		sVo.setNombre(negocio.getSubCategoria().getSubcategoria());
 		sVo.setValid(negocio.getSubCategoria().isValid());
-		
+
 		negociosVo.setSubcategoria(sVo);
 	}
 
