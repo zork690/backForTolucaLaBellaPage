933d97cd669abcff328be8907f210a3558078a43
Cesar Soto <zork_690@hotmail.com>
2025-02-10 14:08:31 -0600

    feat: adding findFavoritos method

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/NegocioDao.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/NegocioDao.java
@@ -12,6 +12,7 @@ public interface NegocioDao extends IGenericDao<Negocio>{
 	//public Usuario validarProveedor(CredencialesVo usuario);
 	long conteoNegocios();
 	Optional<List<Negocio>> findAll();
+	List<Negocio> findFavoritos();
 	Optional<List<Negocio>> findOnlyValids();
 	List<Negocio> getBySubcategoria(String subcategoria);
 	List<Negocio> getByUser(String email);
