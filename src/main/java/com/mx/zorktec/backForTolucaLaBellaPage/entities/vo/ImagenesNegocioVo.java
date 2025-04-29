7d0366d1ab324a32e12cbf99371e443aa70e1d2c
Cesar Soto <zork_690@hotmail.com>
2025-04-28 23:01:28 -0600

    feat: se agrega para recibir imágenes de negocio desde panel socios

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/ImagenesNegocioVo.java
@@ -0,0 +1,23 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImagenesNegocioVo {
+	
+	@NotBlank(message="IdNegocio es requerido")
+	@Pattern(message="IdNegocio solo números", regexp = "^[0-9]+$")
+	private String idNegocio;
+	
+	@Size(min=1, max=10, message = "Imágenes negocio mínimo 1, máximo 10")
+	private List<ImagenesNegociosVo> imagenes;
+
+}
