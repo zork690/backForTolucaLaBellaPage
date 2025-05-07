0f2bb22d4b0777cd19776ec649722fb6f55215f9
Cesar Soto <zork_690@hotmail.com>
2025-05-07 10:13:01 -0600

    feat: se implementa deleteImagen

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/ImagenDaoImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/ImagenDaoImpl.java
@@ -3,6 +3,8 @@ package com.mx.zorktec.backForTolucaLaBellaPage.daos.impl;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.query.Query;
@@ -10,7 +12,6 @@ import org.springframework.stereotype.Repository;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.daos.ImagenDao;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.Imagen;
-import com.mx.zorktec.backForTolucaLaBellaPage.entities.Negocio;
 
 @Repository
 public class ImagenDaoImpl extends GenericDaoImpl<Imagen> implements ImagenDao {
@@ -62,6 +63,15 @@ public class ImagenDaoImpl extends GenericDaoImpl<Imagen> implements ImagenDao {
 		String sql = sb.toString();
 		Query<Imagen> query = super.getSession().createQuery(sql, Imagen.class);
 		return Optional.of(query.getResultList());
+	}
+
+	@Override
+	@Transactional
+	public void deleteImagen(int idImagen, String idNegocio) {
+		String sql = "DELETE FROM Imagen WHERE id = :idImagen AND id_negocio = :idNegocio";
+		super.getSession().createQuery(sql).setParameter("idImagen", idImagen)
+		.setParameter("idNegocio", idNegocio)
+		.executeUpdate();
 	}
 
 }
