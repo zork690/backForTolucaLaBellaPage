34b10f3e06d3e348fcd0c3c60a9fae5d08228de7
Cesar Soto <zork_690@hotmail.com>
2025-05-13 21:15:33 -0600

    feat: se implementa eliminarImagenFromDirectory

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/ImagenesNegocioServiceImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/services/impl/ImagenesNegocioServiceImpl.java
@@ -2,10 +2,10 @@ package com.mx.zorktec.backForTolucaLaBellaPage.services.impl;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +13,6 @@ import javax.xml.bind.DatatypeConverter;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -21,6 +20,7 @@ import org.springframework.stereotype.Service;
 import com.mx.zorktec.backForTolucaLaBellaPage.daos.ImagenDao;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.Imagen;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.Negocio;
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.EliminarImagenNegocioVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenNegocioVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenesNegocioVo;
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.vo.ImagenesNegociosVo;
@@ -38,6 +38,8 @@ public class ImagenesNegocioServiceImpl implements ImagenesNegocioService {
 
 	@Autowired
 	private ImagenDao imagenDao;
+	
+	private static final String NOMBRE_ARCHIVO_ELIMINAR = "imagenAEliminar.txt";
 
 	@Override
 	public void processingImagefromNegocio(Negocio negocio, List<ImagenesNegociosVo> imagenes, String randomId) {
@@ -111,6 +113,31 @@ public class ImagenesNegocioServiceImpl implements ImagenesNegocioService {
 			}
 		});
 
+	}
+	
+	@Override
+	public void eliminarImagenesNegocio(EliminarImagenNegocioVo imagen, TokenPayloadVo tokenInfo) throws Exception {
+		LOG.info("Usuario email: {}", tokenInfo.getEmail());
+		// TODO sacar el id del usuario por su email y hacer select de idNegocios que
+		// tiene el usuario y en caso que no coincida con el idNegocio enviado mandar
+		// una excepción
+		Imagen i = this.imagenDao.getImagenById(Integer.valueOf(imagen.getIdImagen()), imagen.getIdNegocio());
+		LOG.info("Obteniendo imagen: {}", i.getNombre());
+		this.eliminarImagenFromDirectory(i);
+		LOG.info("Eliminando imagen: {}", imagen.getIdImagen());
+		this.imagenDao.deleteImagen(Integer.valueOf(imagen.getIdImagen()), imagen.getIdNegocio());
+		
+	}
+	
+	private void eliminarImagenFromDirectory(Imagen imagen) throws Exception {
+		String filePath = this.basePath + "/" + NOMBRE_ARCHIVO_ELIMINAR;
+		PrintWriter writer = new PrintWriter(
+				new OutputStreamWriter(
+						new FileOutputStream(filePath, true), "UTF-8"
+						));
+		writer.println(imagen.getNombre());
+		writer.close();
+		LOG.info("Archivo para eliminar imagen creado y escrito correctamente.");
 	}
 
 	private void processingImagefromNegocio(Negocio negocio, ImagenesNegociosVo imagen) throws Exception {
