c0cbb72e974511c88585c97392ca57b54e5a65d8
Cesar Soto <zork_690@hotmail.com>
2025-05-20 20:23:39 -0600

    feat: se implementa findComentariosByIdNegocio

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/NegocioComentarioDaoImpl.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/impl/NegocioComentarioDaoImpl.java
@@ -1,5 +1,7 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.daos.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.mx.zorktec.backForTolucaLaBellaPage.daos.NegocioComentarioDao;
@@ -12,6 +14,16 @@ public class NegocioComentarioDaoImpl extends GenericDaoImpl<NegocioComentario> 
 	protected Class<NegocioComentario> getType() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<NegocioComentario> findComentariosByIdNegocio(String idNegocio) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(" FROM NegocioComentario");
+		sb.append(" where id_negocio = ");
+		sb.append(idNegocio);
+		String sql = sb.toString();
+		return super.getSession().createQuery(sql, NegocioComentario.class).getResultList();
 	}
 
 }
