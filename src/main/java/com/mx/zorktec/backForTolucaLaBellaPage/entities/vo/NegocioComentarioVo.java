4e4d7f06d7591d5c96ae2ad95cfce428834e0c9d
Cesar Soto <zork_690@hotmail.com>
2025-05-20 19:58:04 -0600

    feat: para salida de listado de comentarios

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/NegocioComentarioVo.java
@@ -0,0 +1,13 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NegocioComentarioVo {
+	
+	private String comentario;
+	private String nickName;
+
+}
