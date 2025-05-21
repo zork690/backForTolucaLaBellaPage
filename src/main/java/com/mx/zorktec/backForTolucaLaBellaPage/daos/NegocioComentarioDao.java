55940c8251ee596bf88a1e2467fb9642afa1954d
Cesar Soto <zork_690@hotmail.com>
2025-05-20 19:42:57 -0600

    feat: para declarar las queries

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/NegocioComentarioDao.java
@@ -0,0 +1,7 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.daos;
+
+import com.mx.zorktec.backForTolucaLaBellaPage.entities.NegocioComentario;
+
+public interface NegocioComentarioDao extends IGenericDao<NegocioComentario> {
+
+}
