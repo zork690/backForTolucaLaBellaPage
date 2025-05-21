254c8437c395203fcc54bc3fd4ecb930d5910847
Cesar Soto <zork_690@hotmail.com>
2025-05-20 20:18:52 -0600

    feat: se declara findComentariosByIdNegocio

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/NegocioComentarioDao.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/daos/NegocioComentarioDao.java
@@ -1,7 +1,11 @@
 package com.mx.zorktec.backForTolucaLaBellaPage.daos;
 
+import java.util.List;
+
 import com.mx.zorktec.backForTolucaLaBellaPage.entities.NegocioComentario;
 
 public interface NegocioComentarioDao extends IGenericDao<NegocioComentario> {
+	
+	List<NegocioComentario> findComentariosByIdNegocio(String idNegocio);
 
 }
