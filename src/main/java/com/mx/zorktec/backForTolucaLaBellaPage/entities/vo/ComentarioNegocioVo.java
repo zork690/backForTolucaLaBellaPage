a40b097a6018b35d3d19c83a39bab64b963c4309
Cesar Soto <zork_690@hotmail.com>
2025-05-21 13:34:50 -0600

    feat: para recibir el comentario del usuario

---
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/entities/vo/ComentarioNegocioVo.java
@@ -0,0 +1,29 @@
+package com.mx.zorktec.backForTolucaLaBellaPage.entities.vo;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ComentarioNegocioVo {
+	
+	@NotBlank(message="Comentario requerido")
+	@Size(max=50, message = "Comentario máximo 350 carácteres")
+	@Pattern(message="Comentario carácteres no permitidos", regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ$%\"!0-9()?¿=.,#& ]+$")
+	private String comentario;
+	
+	@NotBlank(message="idNegocio requerido")
+	@Pattern(message="idNegocio solo números", regexp = "^[0-9]+$")
+	private String idNegocio;
+	
+	@NotBlank(message="idUsuario requerido")
+	@Pattern(message="idUsuario solo números", regexp = "^[0-9]+$")
+	private String idUsuario;
+
+}
