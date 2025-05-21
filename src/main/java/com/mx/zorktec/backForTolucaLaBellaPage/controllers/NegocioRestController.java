80c4e07b0a5566578b067441ff53994f354b869d
Cesar Soto <zork_690@hotmail.com>
2025-05-20 19:51:25 -0600

    feat: se implementa listarNegocioComentarios

--- src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/controllers/NegocioRestController.java
+++ src/main/java/com/mx/zorktec/backForTolucaLaBellaPage/controllers/NegocioRestController.java
@@ -88,6 +88,19 @@ public class NegocioRestController {
 			return new ResponseEntity<SimpleResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+	@GetMapping("/negocios/listarComentarios/{negocioId}")
+	public ResponseEntity<SimpleResponse> listarNegocioComentarios(@PathVariable String negocioId){
+		SimpleResponse response = new SimpleResponse();
+		try {
+			response.setResult(new ArrayList<NegociosInfoVo>(this.negocioService.getNegocioComentarios()));
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		}catch(Exception error) {
+			LOG.error("Error al consultar los comentarios del negocio: {}", error.getMessage());
+			response.setError(error.getMessage());
+			return new ResponseEntity<SimpleResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	@GetMapping("/negocios/listarNegocios/{negocioId}")
 	public ResponseEntity<SimpleResponse> listarNegocioById(@PathVariable String negocioId){
